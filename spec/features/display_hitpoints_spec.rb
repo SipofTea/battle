760f36d5cb5fe738f2f2dad6fecbf77d6955904c
Else Meijerink <61120785+SipofTea@users.noreply.github.com>
2021-07-15 13:31:41 +0100

    Added helper method for feature tests

--- spec/features/display_hitpoints_spec.rb
+++ spec/features/display_hitpoints_spec.rb
@@ -1,10 +1,7 @@
 
 feature 'Hitpoint display' do
     scenario 'will display hitpoints for player 2' do
-        visit('/')
-        fill_in "player_1",	with: "Scott Pilgrim"
-        fill_in "player_2",	with: "The World"
-        click_button "Submit"
+        sign_in_and_play
 
         expect(page).to have_content "Hitpoints: 10"
     end
--- spec/features/display_names_spec.rb
+++ spec/features/display_names_spec.rb
@@ -1,13 +1,10 @@
 
 feature 'Name display' do
     scenario 'will display names entered' do
-        visit('/')
-        fill_in "player_1",	with: "Scott Pilgrim"
-        fill_in "player_2",	with: "The World"
-        click_button "Submit"
+        sign_in_and_play
 
         save_and_open_page
-        
+
         expect(page).to have_content "Scott Pilgrim vs. The World" 
     end
 end
