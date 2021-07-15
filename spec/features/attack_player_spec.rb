a6e557c007019f28030766e5407f1cf88d92016b
Else Meijerink <61120785+SipofTea@users.noreply.github.com>
2021-07-15 14:43:21 +0100

    Implemented attack link

---
+++ spec/features/attack_player_spec.rb
@@ -0,0 +1,9 @@
+
+feature 'Attack player' do
+    scenario 'attack player 2' do
+        sign_in_and_play
+        click_link "Attack!"
+        expect(page).to have_content "Scott Pilgrim attacked The World!"
+    end
+
+end
--- spec/features/display_names_spec.rb
+++ spec/features/display_names_spec.rb
@@ -3,7 +3,7 @@ feature 'Name display' do
     scenario 'will display names entered' do
         sign_in_and_play
 
-        save_and_open_page
+        #save_and_open_page
 
         expect(page).to have_content "Scott Pilgrim vs. The World" 
     end
