fa526a65ce364afb5a3690874f1435d4656f1845
Else Meijerink <61120785+SipofTea@users.noreply.github.com>
2021-07-16 11:41:13 +0100

    Implemented player attack/reducing hitpoints

--- spec/features/attack_player_spec.rb
+++ spec/features/attack_player_spec.rb
@@ -5,5 +5,10 @@ feature 'Attack player' do
         click_link "Attack!"
         expect(page).to have_content "Scott Pilgrim attacked The World!"
     end
+    scenario 'player 2 hitpoints are reduced by 10' do
+        sign_in_and_play
+        click_link "Attack!"
+        expect(page).to have_content "The World now has 50 hitpoints!"
+    end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,8 +1,16 @@
 require 'player'
 
 describe Player do
-    subject(:player) { Player.new("Scott Pilgrim") }
+    subject(:player_1) { Player.new("Scott Pilgrim") }
+    subject(:player_2) { Player.new("The World") }
     it 'returns its name' do
-        expect(player.name).to eq("Scott Pilgrim")
+        expect(player_1.name).to eq("Scott Pilgrim")
+    end
+    it 'starts with 60 hp' do
+        expect(player_1.hit_points).to eq(60)
+    end
+    it 'reduces hitpoints by 10 when attacking' do
+        player_1.attack(player_2)
+        expect(player_2.hit_points).to eq(50)
     end
 end
